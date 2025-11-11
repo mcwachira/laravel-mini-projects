a48a2d49686eae442f249e418198977e84f31a3d
mcwachira <mcwachira@outlook.com>
2025-11-11 14:22:31 +0300

    Was the able to create ,validate and push a task to the database

--- task-manager/routes/web.php
+++ task-manager/routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
@@ -14,6 +15,8 @@ Route::get('/tasks', function (){
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create') -> name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
 
     return view('show', [
@@ -21,6 +24,28 @@ Route::get('/tasks/{id}', function ($id) {
 
     ]);
 })->name("tasks.show");
+
+
+
+Route::post('/tasks', function (Request $request){
+//    dd($request->all());
+
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task -> save();
+
+    return redirect()->route("tasks.show", ['id', $task->id]);
+
+})->name('tasks.store');
 
 //Route::get('/about', function () {
 //    return 'main hello';
