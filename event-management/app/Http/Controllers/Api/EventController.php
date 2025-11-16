63b19b3e44adb30f90b7310557c0fb9d2364f598
mcwachira <mcwachira@outlook.com>
2025-11-16 10:54:41 +0300

    Was able to create a store an event inside my database

--- event-management/app/Http/Controllers/Api/EventController.php
+++ event-management/app/Http/Controllers/Api/EventController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     //
+
+    public function index(){
+        return Event ::all();
+}
+
+public function store (Request $request){
+
+        $event= Event::create([
+            ...$request ->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string|',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time',
+
+            ]),
+            'user_id' => 1
+        ]);
+
+        return $event;
+
+}
+
+public function show (Event $event){
+        return $event;
+}
 }
