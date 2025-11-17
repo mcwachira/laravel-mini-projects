95b3544b80d30e55360b11d7f8754df65dcff8c4
mcwachira <mcwachira@outlook.com>
2025-11-17 21:50:53 +0300

    Created crud methods for the Attendee api

--- event-management/app/Http/Controllers/Api/AttendeeController.php
+++ event-management/app/Http/Controllers/Api/AttendeeController.php
@@ -3,9 +3,47 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Models\Attendee;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
-    //
+    public function index(Event $event){
+        $attendees = $event->attendees()->latest();
+
+        return AttendeeResource::collection(
+            $attendees -> paginate()
+        );
+    }
+
+
+    public function store (Request $request, Event $event){
+
+        $attendee=$event ->attendees()->create([
+            'user_id' => 1
+        ]);
+
+        return new AttendeeResource($attendee);
+
+    }
+
+    public function show ( Attendee $attendee){
+
+        return new AttendeeResource($attendee);
+    }
+
+    public function destroy ( string $event, Attendee $attendee){
+        $attendee -> delete();
+
+        return response()->json([
+            'message' => 'Event deleted successfully'
+        ]);
+    }
+
 }
+
+
+
+
