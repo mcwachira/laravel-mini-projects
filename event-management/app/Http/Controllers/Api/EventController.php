ef56f3d03104c30f1ea0b6fb9f724aa6d20d94db
mcwachira <mcwachira@outlook.com>
2025-11-17 22:43:18 +0300

    Added ways of loading optional relationships while fetching event data

--- event-management/app/Http/Controllers/Api/EventController.php
+++ event-management/app/Http/Controllers/Api/EventController.php
@@ -12,7 +12,17 @@ class EventController extends Controller
     //
 
     public function index(){
-        return EventResource ::collection(Event::with('user') -> get());
+
+        $query = Event::query();
+      $relations = ['user', 'attendees', 'attendees.user'];
+
+      foreach($relations as $relation){
+          $query -> when (
+              $this-> shouldIncludeRelation($relation),
+              fn($q) => $q->with($relation)
+          );
+      }
+        return EventResource ::collection($query ->latest() -> paginate());
 }
 
 public function store (Request $request){
@@ -30,6 +40,17 @@ public function store (Request $request){
 
         return new EventResource($event);
 
+}
+
+protected function shouldIncludeRelation(string $relation):bool{
+
+        $include = request()->query('include');
+        if(!$include){
+            return false;
+        }
+
+        $relations =array_map('trim', explode(',', $include));
+       return in_array($relation, $relations);
 }
 
 public function show (Event $event){
