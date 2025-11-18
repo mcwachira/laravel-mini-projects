e30e96a34718dd46ddfa332cc7b4c3f9756219bb
mcwachira <mcwachira@outlook.com>
2025-11-18 19:27:49 +0300

    Added functionality to send notifications via email

--- event-management/app/Console/Commands/SendEventReminder.php
+++ event-management/app/Console/Commands/SendEventReminder.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -35,7 +36,7 @@ class SendEventReminder extends Command
 
         $this->info("Found {$eventCount} {$eventLabel} events.");
 
-        $events -> each(fn($event) => $event -> attendees->each(fn($attendee  ) => $this-> info('Notifying the user {$attendee -> user -> id} ')));
+        $events -> each(fn($event) => $event -> attendees->each(fn($attendee) =>$attendee-> user -> notify(new EventReminderNotification($event))));
         $this -> info('notification sent successfully ');
     }
 }
