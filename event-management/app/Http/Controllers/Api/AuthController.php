4a0cc4a4bbd89319d77ec11abfbee48870b356e0
mcwachira <mcwachira@outlook.com>
2025-11-18 14:01:39 +0300

    Added logout functionality

--- event-management/app/Http/Controllers/Api/AuthController.php
+++ event-management/app/Http/Controllers/Api/AuthController.php
@@ -45,5 +45,11 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
 
+        $request -> user() -> tokens() -> delete();
+        return response() ->json([
+            'message' => 'Logged out successfully'
+        ]);
     }
 }
+
+
