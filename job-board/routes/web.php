05061899abce1d749744fecbbecd2092e2bcb798
mcwachira <mcwachira@outlook.com>
2025-12-02 00:57:29 +0300

    Created the Show job blade view to show job details

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', fn() => to_route('jobs.index'));
-Route::resource('jobs', JobController::class)->only(['index']);
+Route::resource('jobs', JobController::class)->only(['index', 'show']);
