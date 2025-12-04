ba7687cfaa4bb0266b28a6e0266064c8398f675c
mcwachira <mcwachira@outlook.com>
2025-12-04 22:29:47 +0300

    Created the Employeer  model, migration and relations

--- job-board/app/Models/Job.php
+++ job-board/app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
@@ -15,6 +16,10 @@ class Job extends Model
 //    typed property
     public static array $experience = ["entry", "intermediate", "senior"];
     public static array $category = ['IT', 'Finance', 'Sales', 'Marketing'];
+    public function employer():BelongsTo
+    {
+            return $this->belongsTo(Employer::class);
+    }
     public function scopeFilter(Builder | QueryBuilder $query, array $filters):Builder | QueryBuilder
     {
 
