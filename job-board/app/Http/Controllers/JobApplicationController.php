b66c6758267ab6ba28dfac93418164c0c5f18310
mcwachira <mcwachira@outlook.com>
2025-12-06 13:26:47 +0300

    Created the Job Controller , routing , form job application and the job application logic

---
+++ job-board/app/Http/Controllers/JobApplicationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use App\Models\JobApplication;
+use Illuminate\Http\Request;
+
+class JobApplicationController extends Controller
+{
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Job $job)
+    {
+        //
+        return view ('job_application.create', ['job' => $job]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Job $job, Request $request)
+    {
+        //
+
+        $job -> jobApplications()-> create([
+            'user_id' => $request -> user() -> id,
+            ...$request->validate([
+                'expected_salary' => 'required|min:1|max:1000000',
+            ])
+        ]);
+
+        return redirect()->route('jobs.show', $job)->with('success', 'Job Application submitted');
+    }
+
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ job-board/resources/views/job_application/create.blade.php
@@ -0,0 +1,32 @@
+<x-layout>
+    <x-bread-crumbs  class="mb-4" :links="[
+    'Jobs' =>route('jobs.index'),
+    $job->title=>route('jobs.show', $job),
+    'Apply' => '#'
+    ]"/>
+
+    <x-job-card :job="$job"/>
+    <x-card>
+
+        <h2 class="mb-4 text-lg-font-medium">
+            Your Job Application
+        </h2>
+
+        <form method="POST" action="{{route('job.application.store', $job)}}">
+        @csrf
+            <div class="mb-4">
+                <label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900">
+Expected Salary
+                </label>
+
+                <x-text-input type="number" name="expected_salary"/>
+            </div>
+
+            <x-button class="w-full">
+
+                Apply
+            </x-button>
+
+        </form>
+    </x-card>
+</x-layout>
